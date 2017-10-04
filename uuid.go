b06ad57e11360db3f0eb4bb0d1101e138aedaa2b
Andrea Limoli <andrealimoli1992@gmail.com>
2017-10-04 09:07:54 +0200

    Simplify method to get UUID as string without dashes.

--- uuid.go
+++ uuid.go
@@ -213,15 +213,7 @@ func (u UUID) String() string {
 // Returns string representation of UUID without dashes:
 // xxxxxxxx(-)xxxx(-)xxxx(-)xxxx(-)xxxxxxxxxxxx.
 func (u UUID) StringWithoutDashes() string {
-	buf := make([]byte, 32)
-
-	hex.Encode(buf[0:8], u[0:4])
-	hex.Encode(buf[8:12], u[4:6])
-	hex.Encode(buf[12:16], u[6:8])
-	hex.Encode(buf[16:20], u[8:10])
-	hex.Encode(buf[20:], u[10:])
-
-	return string(buf)
+	return hex.EncodeToString(u.Bytes())
 }
 
 // SetVersion sets version bits.
